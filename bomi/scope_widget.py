660727e50842fac189141fd1f69833aa8ae5a0dc
Tiger Nie <tnie@wustl.edu>
2022-01-24 16:42:45 -0600

    refactored scope

--- bomi/scope_widget.py
+++ bomi/scope_widget.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
-from typing import Callable, Dict, List
+from typing import ClassVar, Dict, List, Tuple
 from queue import Queue
 from dataclasses import dataclass
 from pathlib import Path
 from timeit import default_timer
 import traceback
 import pyqtgraph.parametertree as ptree
+from pyqtgraph.parametertree.parameterTypes import ActionParameter
 import PySide6.QtGui as qg
 import PySide6.QtWidgets as qw
 import PySide6.QtCore as qc
@@ -15,42 +16,35 @@ import numpy as np
 from bomi.device_manager import DeviceManager, Packet
 
 
-children = [
-    dict(name="streaming", title="Streaming", type="bool", value=True),
-    dict(name="sigopts", title="Signal Options", type="group", children=[]),
-    dict(name="antialias", type="bool", value=pg.getConfigOption("antialias")),
-    dict(
-        name="connect",
-        type="list",
-        limits=["all", "pairs", "finite", "array"],
-        value="all",
-    ),
-    dict(name="skipFiniteCheck", type="bool", value=False),
-]
-
-params = ptree.Parameter.create(name="Parameters", type="group", children=children)
-
-
 def _print(*args):
     print("[ScopeWidget]", *args)
 
 
 @dataclass
 class Buffer:
-    timestamp: np.array
-    data: np.array
+    """Manage all data (packets) consumed from the queue"""
+
+    all_labels: ClassVar = ("Roll", "Pitch", "Yaw", "abs(roll) + abs(pitch)")
+
     ptr: int
+    timestamp: np.array  # 1D array of timestamps
+    data: np.array  # 2D array of (roll, pitch, yaw)
+    labels: List[str] = all_labels
 
     @classmethod
-    def init(cls, initial_buf_size: int, dims: int) -> Buffer:
+    def init(cls, initial_buf_size: int) -> Buffer:
         timestamp = np.zeros(initial_buf_size)
-        buf = np.zeros((initial_buf_size, dims))
-        ptr = 0
-        return Buffer(timestamp, buf, ptr)
+        buf = np.zeros((initial_buf_size, len(cls.all_labels)))
+        return Buffer(ptr=0, timestamp=timestamp, data=buf)
 
     def add(self, packet: Packet):
         data, ts = self.data, self.timestamp
-        data[self.ptr, :2] = (packet.roll, packet.pitch)
+        data[self.ptr, :] = (
+            packet.roll,
+            packet.pitch,
+            packet.yaw,
+            abs(packet.roll) + abs(packet.pitch),
+        )
         ts[self.ptr] = packet.t
         self.ptr += 1
 
@@ -62,29 +56,149 @@ class Buffer:
             self.timestamp = np.empty(l * 2)
             self.timestamp[:l] = ts
 
+    def save(self, fname: str | Path):
+        pass
 
-def init_buffers(names: List[str], bufsize: int, dims: int) -> Dict[str, Buffer]:
-    return {dev: Buffer.init(initial_buf_size=bufsize, dims=dims) for dev in names}
+    @staticmethod
+    def init_buffers(names: List[str], bufsize: int) -> Dict[str, Buffer]:
+        return {dev: Buffer.init(initial_buf_size=bufsize) for dev in names}
+
+
+@dataclass
+class PlotHandle:
+    "Holds a PlotItem and its curves"
+    plot: pg.PlotItem | pg.ViewBox
+    curves: List[pg.PlotCurveItem]
+    target: pg.LinearRegionItem | None
+
+    @classmethod
+    def init(
+        cls, plot: pg.PlotItem, target_range: Tuple[float, float] = None
+    ) -> PlotHandle:
+        "Create curves on the given plot object"
+        # init curves
+        pens = ["r", "g", "b", "w"]
+        curves = [
+            plot.plot(pen=pen, name=name) for pen, name in zip(pens, Buffer.all_labels)
+        ]
+
+        target = cls.init_target(plot, target_range) if target_range else None
+
+        return PlotHandle(plot=plot, curves=curves, target=target)
+
+    @staticmethod
+    def init_target(
+        plot: pg.PlotItem, target_range: Tuple[float, float]
+    ) -> pg.LinearRegionItem:
+        # Target region
+        target = pg.LinearRegionItem(
+            values=target_range, orientation="horizontal", movable=False
+        )
+        pg.InfLineLabel(target.lines[0], "Target", position=0.05, anchor=(1, 1))
+        plot.addItem(target)
+        return target
+
+    def update_target(self, target_range: Tuple[float, float]):
+        if self.target is None:
+            self.target = self.init_target(self.plot, target_range)
+        self.target.lines[0].setValue(target_range[0])
+        self.target.lines[1].setValue(target_range[1])
+
+    def clear_target(self):
+        self.plot.removeItem(self.target)
+        self.target = None
 
 
 class ScopeWidget(qw.QWidget):
-    def __init__(self, device_manager: DeviceManager):
+    def __init__(
+        self,
+        device_manager: DeviceManager,
+        target_show: bool = False,
+        target_range: Tuple[float, float] = (0.8, 0.9),
+        window_title="Scope",
+    ):
         super().__init__()
         self.dm: DeviceManager = device_manager
+        self.setWindowTitle(window_title)
 
-    def show(self) -> None:
+        children = [
+            dict(name="streaming", title="Streaming", type="bool", value=True),
+            ActionParameter(name="tare", title="Tare"),
+            dict(
+                name="target",
+                title="Target",
+                type="group",
+                children=[
+                    dict(name="tshow", title="Show", type="bool", value=target_show),
+                    dict(name="tmax", title="Max", type="float", value=target_range[1]),
+                    dict(name="tmin", title="Min", type="float", value=target_range[0]),
+                ],
+            ),
+        ]
+
+        self.params: ptree.Parameter = ptree.Parameter.create(
+            name="Controls", type="group", children=children
+        )
+
+        def onPTChange(_, changes):
+            for param, change, data in changes:
+                if param.name() == "streaming":
+                    self.toggle_stream(data)
+                elif param.name() in ("tmax", "tmin"):
+                    self.update_targets()
+                elif param.name() == "tshow":
+                    if data == False:
+                        self.clear_targets()
+                    else:
+                        self.update_targets()
+
+        self.params.sigTreeStateChanged.connect(onPTChange)
+
+        def tare():
+            with pg.BusyCursor():
+                self.stop_stream()
+                self.start_stream()
+
+        self.params.child("tare").sigActivated.connect(tare)
+
+        self.queue: Queue[Packet] = Queue()
+
+    def clear_targets(self):
+        for name in self.dev_names:
+            plot_handle = self.plot_handles[name]
+            plot_handle.clear_target()
+
+    def update_targets(self):
+        if not self.get_target_show():
+            return
+        target_range = self.get_target_range()
+        for name in self.dev_names:
+            plot_handle = self.plot_handles[name]
+            plot_handle.update_target(target_range)
+
+    def get_target_show(self) -> bool:
+        return self.params.child("target").child("tshow").value()
+
+    def get_target_range(self) -> Tuple[float, float]:
+        targ_param = self.params.child("target")
+        target_range = (
+            targ_param.child("tmax").value(),
+            targ_param.child("tmin").value(),
+        )
+        return target_range
+
+    def showEvent(self, event: qg.QShowEvent) -> None:
         self.init_data()
         self.init_ui()
-        return super().show()
+        return super().showEvent(event)
 
     def init_data(self):
         ### data
-        self.dims = 2
-        self.queue: Queue[Packet] = Queue()
+        self.queue = Queue()
         self.dev_names: List[str] = self.dm.get_all_sensor_names()
         self.dev_sn: List[str] = self.dm.get_all_sensor_serial()
         self.init_bufsize = 2000
-        self.buffers = init_buffers(self.dev_names, self.init_bufsize, self.dims)
+        self.buffers = Buffer.init_buffers(self.dev_names, self.init_bufsize)
 
     def init_ui(self):
         ### Init UI
@@ -94,50 +208,29 @@ class ScopeWidget(qw.QWidget):
         main_layout.addWidget(splitter)
 
         pt = ptree.ParameterTree(showHeader=False)
-        pt.setParameters(params)
-
-        def change(_, changes):
-            for param, change, data in changes:
-                if param.name() == "streaming":
-                    if data == False:
-                        self.stop_stream()
-                    else:
-                        self.start_stream()
-
-        params.sigTreeStateChanged.connect(change)
-
+        pt.setParameters(self.params)
         splitter.addWidget(pt)
 
-        self.glw = glw = pg.GraphicsLayoutWidget(title="Plot title")
+        self.glw = glw = pg.GraphicsLayoutWidget()
         splitter.addWidget(glw)
 
         ### Init Plots
-        pens = ["r", "g", "b", "w"]
-
-        def _init_curves(plot: pg.PlotItem):
-            "Create `dims` curves on the given plot object"
-            return [plot.plot(pen=pen) for pen in pens[: self.dims]]
-
-        self.plots: Dict[str, pg.PlotItem | pg.ViewBox] = {}
-        self.curves: Dict[str, List[pg.PlotCurveItem]] = {}
+        self.plot_handles: Dict[str, PlotHandle] = {}
         for i, (name, sn) in enumerate(zip(self.dev_names, self.dev_sn)):
-            self.plots[name] = plot = glw.addPlot(row=i + 1, col=0)
-            plot.setXRange(-5, 0)
+            title = f"{sn}" if name == sn else f"{sn} ({name})"
+            plot: pg.PlotItem = glw.addPlot(row=i + 1, col=0, title=title)
+            plot.addLegend(offset=(1, 1))
+            plot.setXRange(-6, 0)
             plot.setYRange(-np.pi, np.pi)
             plot.setLabel("bottom", "Time", units="s")
             plot.setLabel("left", "Euler Angle", units="rad")
-            if name == sn:
-                plot.setTitle(f"{sn}")
-            else:
-                plot.setTitle(f"{sn} ({name})")
             plot.setDownsampling(mode="peak")
-
-            self.curves[name] = _init_curves(plot)
+            self.plot_handles[name] = PlotHandle.init(plot)
 
         # init timer
         self.timer = qc.QTimer()
         self.timer.setInterval(20)
-        self.timer.timeout.connect(self.updatePlot)
+        self.timer.timeout.connect(self.update)
 
         self.start_stream()
 
@@ -146,16 +239,22 @@ class ScopeWidget(qw.QWidget):
         Recreate the queue and buffers
         """
         self.init_data()
-        # self.queue = Queue()
-        # self.buffers = init_buffers(self.dev_names, self.init_bufsize, self.dims)
         self.dm.start_stream(self.queue)
         self.timer.start(0)
 
     def stop_stream(self):
+        """Stop the data stream and update timer"""
         self.dm.stop_stream()
         self.timer.stop()
 
-    def updatePlot(self):
+    def toggle_stream(self, on: bool):
+        self.start_stream() if on else self.stop_stream()
+
+    def update(self):
+        """Update function connected to the timer
+        1. Consume all data currently in the queue
+        2. If successful, update all plots
+        """
         q = self.queue
         qsize = q.qsize()
         if not qsize:
@@ -170,17 +269,20 @@ class ScopeWidget(qw.QWidget):
             _print("[Update Exception]", traceback.format_exc())
 
         else:  # On successful read from queue, update curves
-            now = default_timer()
-            for name in self.dev_names:
-                curves = self.curves[name]
-                buf = self.buffers[name]
-                x = -(now - buf.timestamp[: buf.ptr])
-                for i in range(self.dims):
-                    curves[i].setData(x=x, y=buf.data[: buf.ptr, i])
+            self.update_plots()
+
+    def update_plots(self):
+        now = default_timer()
+        for name in self.dev_names:
+            curves = self.plot_handles[name].curves
+            buf = self.buffers[name]
+            x = -(now - buf.timestamp[: buf.ptr])
+            for i in range(len(buf.all_labels)):
+                curves[i].setData(x=x, y=buf.data[: buf.ptr, i])
 
     def closeEvent(self, event: qg.QCloseEvent) -> None:
         with pg.BusyCursor():
-            _print("Close event called")
+            # _print("Close event called")
             self.stop_stream()
-            _print("Close event done")
+            # _print("Close event done")
         return super().closeEvent(event)
