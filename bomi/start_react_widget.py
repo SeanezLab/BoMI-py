precision and scope bug fix

bomi/start_react_widget.py
@@ -1,7 +1,4 @@
 from __future__ import annotations
-from dataclasses import dataclass, field
-from queue import Queue
-from typing import Callable, Dict, List, Optional, TypeVar
 import traceback
 import PySide6.QtGui as qg
 import PySide6.QtWidgets as qw
@@ -10,9 +7,8 @@ from PySide6.QtCore import Qt
 import pyqtgraph as pg
 import numpy as np
 
-from bomi.device_manager import YostDeviceManager, DeviceT, Packet
-from bomi.scope_widget import ScopeWidget
-from bomi.sr_precision_widget import SRPrecisionWidget
+from bomi.device_manager import YostDeviceManager
+from bomi.scope_widget import ScopeConfig, ScopeWidget
 from bomi.window_mixin import WindowMixin
 
 
@@ -20,32 +16,14 @@ def _print(*args):
     print("[Start React]", *args)
 
 
-@dataclass
-class Buffer:
-    timestamp: np.array
-    data: np.array
-    ptr: int
-
-    @classmethod
-    def init(cls, initial_buf_size: int, dims: int) -> Buffer:
-        timestamp = np.zeros(initial_buf_size)
-        buf = np.zeros((initial_buf_size, dims))
-        ptr = 0
-        return Buffer(timestamp, buf, ptr)
-
-    def add(self, packet: Packet):
-        data, ts = self.data, self.timestamp
-        data[self.ptr, :2] = (packet.roll, packet.pitch)
-        ts[self.ptr] = packet.t
-        self.ptr += 1
-
-        # Double buffer size if full
-        l, dims = data.shape
-        if self.ptr >= l:
-            self.data = np.empty((l * 2, dims))
-            self.data[:l] = data
-            self.timestamp = np.empty(l * 2)
-            self.timestamp[:l] = ts
+precision_config = ScopeConfig(
+    window_title="Precision",
+    target_show=True,
+    yrange=(0, 180),
+    show_roll=False,
+    show_pitch=False,
+    show_yaw=False,
+)
 
 
 class StartReactWidget(qw.QWidget, WindowMixin):
@@ -63,5 +41,15 @@ class StartReactWidget(qw.QWidget, WindowMixin):
         main_layout.addWidget(btn1)
 
     def s_precision_task(self):
-        self.srp = SRPrecisionWidget(self.dm)
-        self.srp.show()
+        dm = self.dm
+        if not dm.has_sensors():
+            return self.error_dialog(
+                "No sensors available. Plug in the devices, then click on 'Discover devices'"
+            )
+
+        try:
+            self._precision = ScopeWidget(dm, config=precision_config)
+            self._precision.showMaximized()
+        except Exception as e:
+            _print(traceback.format_exc())
+            self.dm.stop_stream()