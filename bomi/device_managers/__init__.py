6caa8db1bd4f6932b12705a2306651461fa39c07
Fisher Sun <f.sun@wustl.edu>
2023-06-22 11:48:31 -0500

    Update device_managers package docstring

--- bomi/device_managers/__init__.py
+++ bomi/device_managers/__init__.py
@@ -1,5 +1,5 @@
 """
-Device managers classes and their QWidget wrappers
+Classes for interfacing with devices
 """
 from .protocols import SupportsStreaming, SupportsGetSensorMetadata, SupportsHasSensors
 from .yost_manager import YostDeviceManager
