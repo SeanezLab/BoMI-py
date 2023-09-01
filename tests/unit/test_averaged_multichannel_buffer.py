0864d724ae88144081dffbf510119c842f599cd9
Fisher Sun <f.sun@wustl.edu>
2023-09-01 17:41:05 -0500

    Fix test not passing

--- tests/unit/test_averaged_multichannel_buffer.py
+++ tests/unit/test_averaged_multichannel_buffer.py
@@ -22,18 +22,22 @@ def test_saves_correct_data(tmp_path, multichannel_data_file):
         channel_labels=channel_labels
     )
 
-    with open(multichannel_data_file) as f:
-        expected = np.genfromtxt(multichannel_data_file, delimiter=",", skip_header=1)
+    expected = np.genfromtxt(multichannel_data_file, delimiter=",", skip_header=1)
 
-        for row in expected:
-            packet = Packet(
-                time=row[0],
-                device_name="1",
-                channel_readings=dict(zip(channel_labels, row[1:]))
-            )
-            buffer.add_packet(packet)
+    for row in expected:
+        packet = Packet(
+            time=row[0],
+            device_name="1",
+            channel_readings=dict(zip(channel_labels, row[1:]))
+        )
+        buffer.add_packet(packet)
 
-    actual = np.genfromtxt(buffer.save_file, delimiter=",", skip_header=1)
+    # We need to delete the buffer so that it can close its file pointer and finish writing.
+    # Before that, get the name of its save file.
+    save_file = buffer.save_file
+    del buffer
+
+    actual = np.genfromtxt(save_file, delimiter=",", skip_header=1)
     expected = np.genfromtxt(multichannel_data_file, delimiter=",", skip_header=1)
     assert(np.array_equal(actual, expected))
 
