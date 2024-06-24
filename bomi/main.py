5aeed2d3131600090d1a81dc42a448fb6fcd113c
zachmichael14 <zachmichael14@gmail.com>
2024-06-24 13:12:45 -0500

    Prompt for save dir an SID before opening app

--- bomi/main.py
+++ bomi/main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import PySide6.QtGui as qg
 import PySide6.QtWidgets as qw
 
@@ -9,7 +11,7 @@ from bomi.device_managers.qtm_widget import QtmWidget
 
 from bomi.start_react_widget import StartReactWidget
 from bomi.window_mixin import WindowMixin
-from bomi.base_widgets import wrap_gb
+from bomi.base_widgets import wrap_gb, ConfirmationDialog
 
 from bomi.device_managers.trigno_widget import TrignoWidget, TrignoClient
 
@@ -28,6 +30,7 @@ class MainWindow(qw.QMainWindow, WindowMixin):
         self.trigno_client = TrignoClient()
         self.qtm_dm = QtmDeviceManager()
 
+        self.prompt_for_save_dir_name()
         self.init_ui()
         self.init_actions()
         # menu bar overlaps with the device manager on Windows
@@ -57,25 +60,25 @@ class MainWindow(qw.QMainWindow, WindowMixin):
 
         ### Device manager group
         ### YOST IMU mamanger group
-        _gbIMU = wrap_gb("Yost devices", YostWidget(self.yost_dm))
+        _gbIMU = wrap_gb("Yost devices", YostWidget(self.save_dir, self.yost_dm))
         _gbIMU.setSizePolicy(qw.QSizePolicy.Expanding, qw.QSizePolicy.Fixed)
         tabs.addTab(_gbIMU, "Yost")
         #vbox1.addWidget(tabs)
 
         ### Trigno Device manager group
-        _gbTrigno = wrap_gb("Trigno devices", TrignoWidget(self.trigno_client))
+        _gbTrigno = wrap_gb("Trigno devices", TrignoWidget(self.save_dir, self.trigno_client))
         tabs.addTab(_gbTrigno, "Trigno")
 
         ### Biodex manager group
-        _gbBiodex = wrap_gb("Biodex devices", QtmWidget(self.qtm_dm))
+        _gbBiodex = wrap_gb("Biodex devices", QtmWidget(self.save_dir, self.qtm_dm))
         _gbBiodex.setSizePolicy(qw.QSizePolicy.Expanding, qw.QSizePolicy.Fixed)
         tabs.addTab(_gbBiodex, "Biodex")
 
-        vbox1.addWidget(tabs) #adds tab widget to vertical box layout 1
+        vbox1.addWidget(tabs) # adds tab widget to vertical box layout 1
 
         ### StartReact manager group
         vbox2.addWidget(
-            wrap_gb("StartReact", StartReactWidget([self.yost_dm, self.qtm_dm], self.trigno_client))
+            wrap_gb("StartReact", StartReactWidget(self.save_dir, [self.yost_dm, self.qtm_dm], self.trigno_client))
         )
    
     def init_actions(self):
@@ -99,8 +102,15 @@ class MainWindow(qw.QMainWindow, WindowMixin):
     def closeEvent(self, event: qg.QCloseEvent) -> None:
         #self.cursor_control.stop()
         return super().closeEvent(event)
+    
+    def prompt_for_save_dir_name(self):
+        confirmation_dialog = ConfirmationDialog(parent=self)
+        confirmation_dialog.sig_save.connect(self.on_confirmation)
+        confirmation_dialog.exec()
 
-
+    def on_confirmation(self, save_dir: Path):
+        self.save_dir = Path(save_dir)
+    
 def main():
     app = qw.QApplication()
     win = MainWindow()
